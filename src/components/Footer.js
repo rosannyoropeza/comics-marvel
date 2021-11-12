5cba60fcf13e2cf358f4b60afba2d040c2d1ce68
rosannyoropeza <rosanny.oropeza82@gmail.com>
2021-11-12 10:35:18 -0500

    feat(Footer): add links to social media icons. Closes #5

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,26 +3,35 @@ import facebook from "../assets/images/facebook.png";
 import instagram from "../assets/images/instagram.png";
 import gorjeo from "../assets/images/gorjeo.png";
 import {
-  BoxFooter,
-  Container,
-  Column,
-  Paragraph,
+    BoxFooter,
+    Container,
+    Column,
+    Paragraph,
 } from "../stylesComponents/FooterStyle";
 
 function Footer() {
-  return (
-    <BoxFooter>
-      <Container>
-        <Paragraph> Visita la página oficial de Marvel</Paragraph>
-        <Column>
-          <img src={facebook} alt="" width="50" height="50" />
-          <img src={instagram} alt="" width="50" height="50" />
-          <img src={gorjeo} alt="" width="50" height="50" />
-        </Column>
-        <Paragraph> © 2021 Rosanny Oropeza. All rights reserved</Paragraph>
-      </Container>
-    </BoxFooter>
-  );
+    return (
+        <BoxFooter>
+            <Container>
+                <Paragraph> Visita la página oficial de Marvel</Paragraph>
+                <Column>
+                    <a rel="noopener noreferrer" href="http://facebook.com/marvel" target="_blank">
+                        <img src={facebook} alt="" width="50" height="50" />
+                    </a>
+                    <a rel="noopener noreferrer" href="http://instagram.com/marvel" target="_blank">
+                        <img src={instagram} alt="" width="50" height="50" />
+                    </a>
+                    <a rel="noopener noreferrer" href="http://twitter.com/marvel" target="_blank">
+                        <img src={gorjeo} alt="" width="50" height="50" />
+                    </a>
+                </Column>
+                <Paragraph>
+                    {" "}
+                    © 2021 Rosanny Oropeza. All rights reserved
+                </Paragraph>
+            </Container>
+        </BoxFooter>
+    );
 }
 
 export default Footer;
